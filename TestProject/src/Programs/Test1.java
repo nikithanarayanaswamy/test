2615608d8d36983094d8bd823fdf5e7876d05bad
nikithanarayanaswamy <45616671+nikithanarayanaswamy@users.noreply.github.com>
2018-12-07 11:59:38 +0530

    Update Test1.java

--- TestProject/src/Programs/Test1.java
+++ TestProject/src/Programs/Test1.java
@@ -5,12 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("git test");
 		
-
-		//changed in local
-		
-		
-
-		//changed in chr
+//added in master
 
 	}
 }
